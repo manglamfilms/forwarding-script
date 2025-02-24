c7c364b75615bb2b8ca41949fba3af3afcd79f56
manglamfilms <gra888038@gmail.com>
2025-02-24 11:24:36 +0530

    Create config.py

---
+++ python script telegrm/config.py
@@ -0,0 +1,5 @@
+# config.py
+
+API_ID = 21908775  # अपना API ID डालें
+API_HASH = "2b485c943501502a6e01d9a221084209"  # अपना API Hash डालें
+USERS_FILE = "users.json"  # सभी यूजर्स की जानकारी सेव करने के लिए फाइल
